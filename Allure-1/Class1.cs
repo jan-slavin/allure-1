fa81c999563c43f327881562a9725765ee62f602
belyaev <janslavin@gmail.com>
2014-07-09 02:59:15 +0400

    1

---
+++ Allure-1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AllureCSharpCommons.AllureModel;
+using NUnit.Framework;
+
+namespace Allure_1
+{
+    
+    public class Class1
+    {
+        //[all]
+        //public File File()
+        //{
+
+        //}
+
+
+        [Test]
+        public void Do1()
+        {
+            Assert.AreEqual("10", "10");
+        }
+        [Test]
+        public void Do2()
+        {
+            Assert.AreEqual("5", "6");
+        }
+        [Test]
+        public void Do3()
+        {
+            Assert.AreEqual("7", "7");
+        }
+    }
+}
